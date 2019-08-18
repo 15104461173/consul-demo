4d82b3d4fba3f0a19af5cd9c527c2d2f7373814d
Bowen Song <song825438760@163.com>
2019-08-18 14:55:50 +0800

    modify config file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
 FROM openjdk:8-jdk-alpine3.7
-
 ADD target/*.jar /app.jar
-
 EXPOSE 8080
--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ docker run -d \
 --name=${container_name} \
 --restart=always \
 --net=host \
--e SERVICE_8080_CHECK_HTTP=/healthy \
--e SERVICE_8080_NAME=${container_name} \
+-p 8080:8080 \
+-e SERVICE_CHECK_HTTP=/healthy \
 -e SERVICE_CHECK_INTERVAL=5s \
 -e SERVICE_CHECK_TIMEOUT=2s \
 -e SERVICE_TAGS="urlprefix-/${container_name} strip=/${container_name}" \
