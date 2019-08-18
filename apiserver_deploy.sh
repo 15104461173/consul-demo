f05ffa7fd5e0b3f92882533cf74f5d053e70a307
Bowen Song <song825438760@163.com>
2019-08-18 21:21:58 +0800

    a

--- apiserver_deploy.sh
+++ apiserver_deploy.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-docker_image=$1
 container_name='hostname-server'
-echo "remove docker container:"${container_name}
+docker_image='15104461173/consul-demo'
+echo "remove docker container"
 docker stop ${container_name}
 docker rm ${container_name}
-echo "remove docker image:"${docker_image}
-docker rmi ${docker_image}
+echo "pull docker image"
+docker pull ${docker_image}
 echo "run docker container"
-docker run -d \
---name=${container_name} \
---restart=always \
---net=host \
--p 8080:8080 \
+docker run -d --name=${container_name} --restart=always --net=host -p 8080:8080 \
 -e SERVICE_NAME=${container_name} \
 -e SERVICE_CHECK_HTTP=/healthy \
 -e SERVICE_CHECK_INTERVAL=5s \
--- consulserver_fabio_registrator_deploy.sh
+++ consulserver_fabio_registrator_deploy.sh
@@ -2,7 +2,14 @@
 ipaddr=$(ifconfig enp0s3|awk '{if(NR==2){print $2}}')
 echo "ip address ${ipaddr}"
 echo "start consul server"
+if [ x$1 != x ];then
+    #with another node to join
+echo "try join $1"
+docker run -d --name=consul-server --restart=always --net=host consul agent -server -ui -bind=${ipaddr} -retry-join=$1 -client ${ipaddr}' 127.0.0.1'
+else
+    #without another node to join
 docker run -d --name=consul-server --restart=always --net=host consul agent -server -ui -bind=${ipaddr} -bootstrap-expect=1 -client ${ipaddr}' 127.0.0.1'
+fi
 sleep 5
 echo "start fabio"
 docker run -d --name=fabio --restart=always --net=host fabiolb/fabio
